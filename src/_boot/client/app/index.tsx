9053c963fa7d6b6d4667d811b807efeb6f8c240d
SSen <i@ssen.name>
2018-06-21 14:58:56 +0900

    Update ssenpack 2018.3

--- src/_boot/client/app/index.tsx
+++ src/_boot/client/app/index.tsx
@@ -31,18 +31,4 @@ if (module.hot) {
   Error.stackTraceLimit = Infinity;
   console.clear();
   module.hot.accept();
-  
-  const reporter: {success: () => void} = window['__webpack_hot_middleware_reporter__'];
-  const success: () => void = reporter.success;
-  reporter.success = () => {
-    // reload css
-    const links: NodeListOf<HTMLLinkElement> = document.querySelectorAll('link[href][rel=stylesheet]') as NodeListOf<HTMLLinkElement>;
-    let f: number = links.length;
-    while (--f >= 0) {
-      const link: HTMLLinkElement = links[f];
-      link.href = link.href.replace(/(\?\d+)?$/, `?${Date.now()}`);
-    }
-    
-    success();
-  };
 }
