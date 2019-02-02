2a13eea96f72892b1a6c779e72847d71ecab99eb
SSen <i@ssen.name>
2019-02-02 20:51:23 +0900

    Add script `surge.publish`

---
+++ dist/web/6.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{339:function(e,n,t){"use strict";t.r(n);var a=t(0),i=t.n(a),r=t(95);n.default=function(){var e=Object(r.b)().formatMessage;return i.a.createElement("div",null,e({id:"app.main.text"}))}}}]);
