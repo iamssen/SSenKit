0c8e29cb308bf9a6c32da68d65761f2d7a82d850
SSen <i@ssen.name>
2019-02-04 16:51:51 +0900

    Update zeroconfig

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,12 @@
 import { Layout } from 'antd';
+import 'antd/dist/antd.css';
+import 'prismjs/themes/prism-okaidia.css';
 import React from 'react';
 import { RouteStore } from 'react-router-store';
 import { RouterContents } from './components/RouterContents';
 import { RouterNavigation } from './components/RouterNavigation';
 import './index.less';
 import styles from './index.module.less';
-import 'antd/dist/antd.css';
-import 'prismjs/themes/prism-okaidia.css';
 
 const {Content, Header} = Layout;
 
