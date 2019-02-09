6e307f1c511457cb014176ca669daa3dd25e1ed9
SSen <i@ssen.name>
2019-02-09 19:53:26 +0900

    Publish to surge

--- dist/web/11.js
+++ dist/web/11.js
@@ -1,1 +1,1 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[11],{495:function(e,n,t){"use strict";t.r(n);var a=t(1),i=t.n(a),r=t(166);n.default=function(){var e=Object(r.b)().formatMessage;return i.a.createElement("div",null,e({id:"app.main.text"}))}}}]);
+(window.webpackJsonp=window.webpackJsonp||[]).push([[11],{478:function(e,n,t){"use strict";t.r(n);var a=t(1),i=t.n(a),r=t(157);n.default=function(){var e=Object(r.b)().formatMessage;return i.a.createElement("div",null,e({id:"app.main.text"}))}}}]);
